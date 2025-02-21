c8fb906a7e4d3d21cd1e328d6bef41062bd7f57e
Arijit Ganguly <aganguly158@gmail.com>
2025-02-22 04:01:01 +0530

    api live check is created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,9 @@ import v1Route from './v1';
 
 async function apiRoute(fastify: FastifyInstance) {
     fastify.register(v1Route, { prefix: '/v1' });
+    fastify.get('/check', (_req, res) => {
+        return res.send({ msg: 'alive' });
+    });
 }
 
 export default apiRoute;
