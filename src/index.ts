0bd926d608ca0ac84b9f92d603ac48272ee41b9b
Arijit Ganguly <aganguly158@gmail.com>
2025-02-23 19:10:00 +0530

    cors updated

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,6 @@ const fastify = Fastify();
 fastify.register(cors, {
     origin: ['https://www.workr.club'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 });
 
