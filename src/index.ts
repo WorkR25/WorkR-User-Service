c354e9c5681b3fbddf3e5d23834a40f7c3849536
Arijit Ganguly <aganguly158@gmail.com>
2025-02-23 14:14:57 +0530

    cors updated

--- src/index.ts
+++ src/index.ts
@@ -26,14 +26,6 @@ fastify.register(cors, {
     credentials: true
 });
 
-fastify.options('*', async (_req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://www.workr.club')
-        .header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        .header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-        .header('Access-Control-Allow-Credentials', 'true')
-        .send();
-});
-
 fastify.register(fastifyCookie);
 
 fastify.register(fastifyMultipart, {
